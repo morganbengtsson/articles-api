cbf09c61923940fcd6ccf141f1f3c093c63084cd
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-02-25 10:48:08 +0100

    Add seed data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ArticlesApi.Repo
+alias ArticlesApi.Articles.Article
+
+Repo.insert! %Article{title: "Article 1", description: "This is an article.", body: "Article 1 body text.", published_date: ~D[2020-01-01]}
+Repo.insert! %Article{title: "Article 2", description: "This is an article.", body: "Article 2 body text.", published_date: ~D[2020-01-02]}
+Repo.insert! %Article{title: "Article 3", description: "This is an article.", body: "Article 3 body text.", published_date: ~D[2020-01-03]}
+Repo.insert! %Article{title: "Article 4", description: "This is an article.", body: "Article 4 body text.", published_date: ~D[2020-01-04]}
+Repo.insert! %Article{title: "Article 5", description: "This is an article.", body: "Article 5 body text.", published_date: ~D[2020-01-05]}
