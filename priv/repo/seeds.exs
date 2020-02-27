25bdb52b254dc911e60d5e3dbd392c4a22ab2048
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-02-27 11:10:15 +0100

    Fix seed.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,16 +14,16 @@ alias ArticlesApi.Repo
 alias ArticlesApi.Articles.Article
 alias ArticlesApi.Authors.Author
 
-Repo.insert! %Author{id: 1, first_name: "John", last_name: "Doe", age: 25}
-Repo.insert! %Author{id: 2, first_name: "Jane", last_name: "Doe", age: 49}
+author0 = Repo.insert! %Author{first_name: "John", last_name: "Doe", age: 25}
+author1 = Repo.insert! %Author{first_name: "Jane", last_name: "Doe", age: 49}
 
 date = ~D[2000-01-01]
 
-Repo.insert! %Article{id: 1, author_id: 1, title: "Article 1", description: "This is an article.", body: "Article 1 body text.", published_date: date}
-Repo.insert! %Article{id: 2, author_id: 1, title: "Article 2", description: "This is an article.", body: "Article 2 body text.", published_date: date}
-Repo.insert! %Article{id: 3, author_id: 1, title: "Article 3", description: "This is an article.", body: "Article 3 body text.", published_date: date}
-Repo.insert! %Article{id: 4, author_id: 2, title: "Article 4", description: "This is an article.", body: "Article 4 body text.", published_date: date}
-Repo.insert! %Article{id: 5, author_id: 2, title: "Article 5", description: "This is an article.", body: "Article 5 body text.", published_date: date}
+Repo.insert! %Article{author_id: author0.id, title: "Article 1", description: "This is an article.", body: "Article 1 body text.", published_date: date}
+Repo.insert! %Article{author_id: author0.id, title: "Article 2", description: "This is an article.", body: "Article 2 body text.", published_date: date}
+Repo.insert! %Article{author_id: author0.id, title: "Article 3", description: "This is an article.", body: "Article 3 body text.", published_date: date}
+Repo.insert! %Article{author_id: author1.id, title:  "Article 4", description: "This is an article.", body: "Article 4 body text.", published_date: date}
+Repo.insert! %Article{author_id: author1.id, title: "Article 5", description: "This is an article.", body: "Article 5 body text.", published_date: date}
 
 
 
