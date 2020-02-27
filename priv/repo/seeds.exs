cbbef24aa3cf453daaef052dd9f7af9d26ced662
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-02-27 10:28:25 +0100

    Date.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -17,13 +17,13 @@ alias ArticlesApi.Authors.Author
 Repo.insert! %Author{id: 1, first_name: "John", last_name: "Doe", age: 25}
 Repo.insert! %Author{id: 2, first_name: "Jane", last_name: "Doe", age: 49}
 
-utc = DateTime.now("Etc/UTC")|> elem(1) |> DateTime.truncate(:second)
+date = ~D[2000-01-01]
 
-Repo.insert! %Article{id: 1, author_id: 1, title: "Article 1", description: "This is an article.", body: "Article 1 body text.", published_date: utc}
-Repo.insert! %Article{id: 2, author_id: 1, title: "Article 2", description: "This is an article.", body: "Article 2 body text.", published_date: utc}
-Repo.insert! %Article{id: 3, author_id: 1, title: "Article 3", description: "This is an article.", body: "Article 3 body text.", published_date: utc}
-Repo.insert! %Article{id: 4, author_id: 2, title: "Article 4", description: "This is an article.", body: "Article 4 body text.", published_date: utc}
-Repo.insert! %Article{id: 5, author_id: 2, title: "Article 5", description: "This is an article.", body: "Article 5 body text.", published_date: utc}
+Repo.insert! %Article{id: 1, author_id: 1, title: "Article 1", description: "This is an article.", body: "Article 1 body text.", published_date: date}
+Repo.insert! %Article{id: 2, author_id: 1, title: "Article 2", description: "This is an article.", body: "Article 2 body text.", published_date: date}
+Repo.insert! %Article{id: 3, author_id: 1, title: "Article 3", description: "This is an article.", body: "Article 3 body text.", published_date: date}
+Repo.insert! %Article{id: 4, author_id: 2, title: "Article 4", description: "This is an article.", body: "Article 4 body text.", published_date: date}
+Repo.insert! %Article{id: 5, author_id: 2, title: "Article 5", description: "This is an article.", body: "Article 5 body text.", published_date: date}
 
 
 
