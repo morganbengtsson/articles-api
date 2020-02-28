8c3836993aca8d8eb11a1d48f758207e8a563116
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2020-02-28 11:07:30 +0100

    Debug errors.

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :articles_api, ArticlesApi.Repo,
 # with webpack to recompile .js and .css sources.
 config :articles_api, ArticlesApiWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
